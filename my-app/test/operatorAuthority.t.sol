3b8b9c4fcd8102045a3684fa209160fd64adf78a
Fooooooooooox <crunchy.wafer.stick787@gmail.com>
2022-06-27 00:43:01 +0800

    test csb operator can post note

--- my-app/test/operatorAuthority.t.sol
+++ my-app/test/operatorAuthority.t.sol
@@ -24,10 +24,33 @@ contract OperatorAuthorityTest is Test {
         bytes memory bs = new bytes(0);
         DataTypes.CreateCharacterData memory characterData = DataTypes.CreateCharacterData(_this, _handle, _uri, _zero, bs);
         web3entry.createCharacter(characterData);
-        vm.prank(address(1337));  
+        web3entry.setOperator(characterId, address(17));
+        vm.prank(address(17));  
         vm.expectRevert("NotCharacterOwner");
         web3entry.setHandle(characterId, newHandle); 
     }
 
+    function testOperatorCanPostNote() public {
+        web3entry = new Web3Entry();
+        address _this = address(this);
+        address _zero = address(0);
+        string memory _handle = "0xcrossbell-eth";
+        string memory _uri = "https://raw.githubusercontent.com/Crossbell-Box/Crossbell-Contracts/main/examples/sampleProfile.json";
+        bytes memory bs = new bytes(0);
+        DataTypes.CreateCharacterData memory characterData = DataTypes.CreateCharacterData(_this, _handle, _uri, _zero, bs);
+        web3entry.createCharacter(characterData);
+        web3entry.setOperator(characterId, address(17));
+        vm.prank(address(17));  
+
+        uint256 _characterId = 1;
+        string memory _contentUri = "https://github.com/Crossbell-Box/Crossbell-Contracts/blob/main/examples/sampleContent.json";
+        address _linkModule = address(0);
+        bytes memory _linkModuleInitData = new bytes(0);
+        address _mintModule = address(0);
+        bytes memory _mintModuleInitData = new bytes(0);
+        bool _locked = false;
+        DataTypes.PostNoteData memory postnotedata = DataTypes.PostNoteData(_characterId, _contentUri, _linkModule, _linkModuleInitData, _mintModule, _mintModuleInitData, _locked);
+        web3entry.postNote(postnotedata);
+    }
 
 }

1d0ffc1d7a670674ff3449191e201d13cd34476e
Fooooooooooox <crunchy.wafer.stick787@gmail.com>
2022-11-26 23:25:36 +0800

    Bitwiseb shift

--- my_proj/test/ParticipatedWithBitmap.t.sol
+++ my_proj/test/ParticipatedWithBitmap.t.sol
@@ -24,4 +24,37 @@ contract ParticipatedWithBitmapTest is Test {
         assert(!participatedWithBitmap.hasParticipated(0, 2));
 
     }
+
+    function testBitwise() public {
+        uint x = 234; // 234： 11101010
+        uint last3 = getLastNBits(x, 3); // 010: 2
+        console.log(last3);
+
+        uint256 det = ~uint256(0) ;
+        console.logBytes(toBytes(det));
+        console.logBytes(toBytes(~uint256(0) >> 20));        
+    }
+
+    // Get last n bits from x
+    function getLastNBits(uint x, uint n) public pure returns (uint) {
+        // Example, last 3 bits
+        // x        = 1101 = 13
+        // mask     = 0111 = 7
+        // x & mask = 0101 = 5
+        uint mask = (1 << n) - 1;
+        return x & mask;
+    }
+
+    // Get last n bits from x using mod operator
+    function getLastNBitsUsingMod(uint x, uint n) public pure returns (uint) {
+        // 1 << n = 2 ** n
+        return x % (1 << n);
+    }
+
+    function toBytes(uint256 x) public returns (bytes memory b) {
+        b = new bytes(32);
+        assembly { mstore(add(b, 32), x) }
+    }
+
+
 }
